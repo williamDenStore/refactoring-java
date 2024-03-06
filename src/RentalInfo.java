6ae06dfe7b1d582a553c78db6895258bd614f399
William Karlsson <william.karlsson@etraveligroup.com>
2024-03-06 15:46:05 +0100

    create a switch to determine price for each movie

--- src/RentalInfo.java
+++ src/RentalInfo.java
@@ -3,11 +3,7 @@ import java.util.HashMap;
 public class RentalInfo {
 
   public String statement(Customer customer) {
-    HashMap<String, Movie> movies = new HashMap<>();
-    movies.put("F001", new Movie("You've Got Mail", "regular"));
-    movies.put("F002", new Movie("Matrix", "regular"));
-    movies.put("F003", new Movie("Cars", "childrens"));
-    movies.put("F004", new Movie("Fast & Furious X", "new"));
+    HashMap<String, Movie> movies = generateMovieMap();
 
     double totalAmount = 0;
     int frequentEnterPoints = 0;
@@ -16,31 +12,18 @@ public class RentalInfo {
       double thisAmount = 0;
       String currentMovie = movieRental.movieId();
       // determine amount for each movie
-      if (movies.get(currentMovie).code().equals("regular")) {
-        thisAmount = 2;
-        if (movieRental.days() > 2) {
-          thisAmount = ((movieRental.days() - 2) * 1.5) + thisAmount;
-        }
-      }
-
-      if (movies.get(currentMovie).code().equals("new")) {
-        thisAmount = movieRental.days() * 3;
-      }
-      if (movies.get(currentMovie).code().equals("childrens")) {
-        thisAmount = 1.5;
-        if (movieRental.days() > 3) {
-          thisAmount = ((movieRental.days() - 3) * 1.5) + thisAmount;
-        }
-      }
-
+      thisAmount = switch (movies.get(currentMovie).code()) {
+        case "regular" -> regularMoviePrice(movieRental);
+        case "new" -> newMoviePrice(movieRental);
+        case "childrens" -> childrensMoviePrice(movieRental);
+        default -> thisAmount;
+      };
       //add frequent bonus points
       frequentEnterPoints++;
       // add bonus for a two day new release rental
       if (movies.get(currentMovie).code().equals("new") && movieRental.days() > 2){
         frequentEnterPoints++;
       }
-
-
       //print figures for this rental
       result.append("\t").append(movies.get(currentMovie).title()).append("\t").append(thisAmount).append("\n");
       totalAmount = totalAmount + thisAmount;
@@ -50,5 +33,38 @@ public class RentalInfo {
     result.append("You earned ").append(frequentEnterPoints).append(" frequent points\n");
 
     return result.toString();
+  }
+
+  private static double childrensMoviePrice(MovieRental movieRental) {
+    double thisAmount;
+    thisAmount = 1.5;
+    if (movieRental.days() > 3) {
+      thisAmount = ((movieRental.days() - 3) * 1.5) + thisAmount;
+    }
+    return thisAmount;
+  }
+
+  private static double newMoviePrice(MovieRental movieRental) {
+    double thisAmount;
+    thisAmount = movieRental.days() * 3;
+    return thisAmount;
+  }
+
+  private static double regularMoviePrice(MovieRental movieRental) {
+    double thisAmount;
+    thisAmount = 2;
+    if (movieRental.days() > 2) {
+      thisAmount = ((movieRental.days() - 2) * 1.5) + thisAmount;
+    }
+    return thisAmount;
+  }
+
+  private static HashMap<String, Movie> generateMovieMap() {
+    HashMap<String, Movie> movies = new HashMap<>();
+    movies.put("F001", new Movie("You've Got Mail", "regular"));
+    movies.put("F002", new Movie("Matrix", "regular"));
+    movies.put("F003", new Movie("Cars", "childrens"));
+    movies.put("F004", new Movie("Fast & Furious X", "new"));
+    return movies;
   }
 }
