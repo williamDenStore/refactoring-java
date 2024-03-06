03363b4e92c9bf4f26b8e4b10092737ac09c55d0
William Karlsson <william.karlsson@etraveligroup.com>
2024-03-06 14:55:54 +0100

    Separate nestled objects to make it more readable

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,12 @@
-import java.util.Arrays;
+import java.util.List;
 
 public class Main {
 
   public static void main(String[] args) {
     String expected = "Rental Record for C. U. Stomer\n\tYou've Got Mail\t3.5\n\tMatrix\t2.0\nAmount owed is 5.5\nYou earned 2 frequent points\n";
-
-    String result = new RentalInfo().statement(new Customer("C. U. Stomer", Arrays.asList(new MovieRental("F001", 3), new MovieRental("F002", 1))));
+    List<MovieRental> rentedMovies = List.of(new MovieRental("F001", 3), new MovieRental("F002", 1));
+    Customer customer = new Customer("C. U. Stomer", rentedMovies);
+    String result = new RentalInfo().statement(customer);
 
     if (!result.equals(expected)) {
       throw new AssertionError("Expected: " + System.lineSeparator() + String.format(expected) + System.lineSeparator() + System.lineSeparator() + "Got: " + System.lineSeparator() + result);
